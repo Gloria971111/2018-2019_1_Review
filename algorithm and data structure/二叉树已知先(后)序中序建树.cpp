57d250eac55858783ed425f9702c823c58c4cec6
Gloria971111 <36186066+Gloria971111@users.noreply.github.com>
2019-03-31 13:24:25 +0800

    update

--- algorithm and data structure/二叉树已知先(后)序中序建树.cpp
+++ algorithm and data structure/二叉树已知先(后)序中序建树.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 using namespace std;
+#include<queue>
 
+//https://pintia.cn/problem-sets/994805046380707840/problems/994805069361299456
 typedef struct Node{
 	int value;
 	Node* lson;
 	Node* rson;
+	int step; 
 	Node(){
 		lson=NULL;
 		rson=NULL;
@@ -27,10 +30,8 @@ Node* BinaryTreeOrderings1(int af,int in,int len)
 		return NULL;
 	if((af>=n)||(af<0))
 		return NULL;
-	cout<<af<<" "<<in<<" "<<len<<"hi\n";
-	Node* node;
+	Node* node=new Node(); //声明指针的时候一定要记得new这个变量 
 	node->value=afterorder[af];
-	cout<<node->value<<endl;
 	int index;
 	//在中序遍历中查找当前点的位置 
 	for(index=0;index<len;index++)
@@ -38,7 +39,8 @@ Node* BinaryTreeOrderings1(int af,int in,int len)
 		if(inorder[in+index]==afterorder[af])
 			break;
 	}
-	node->lson=BinaryTreeOrderings1(af-index-1,in,index);//左边为左子树 
+	
+	node->lson=BinaryTreeOrderings1(af-len+index,in,index);//左边为左子树 	
 	node->rson=BinaryTreeOrderings1(af-1,in+index+1,len-(index+1));//右边为右子树 
 	return node;
 }
@@ -76,6 +78,37 @@ void preordering(Node* root)
 	}
 }
 
+void afterordering(Node* root)
+{
+	if(root!=NULL){		
+		afterordering(root->lson);
+		afterordering(root->rson);
+		cout<<root->value;
+	}
+}
+int ans[35];
+int num=0;
+
+void Cengordering(Node* root)
+{
+	queue<Node*>q;
+	q.push(root);
+	while(!q.empty())
+	{
+		Node* now=q.front();
+		q.pop();
+		if(now->lson!=NULL)
+			q.push(now->lson);
+		if(now->rson!=NULL)
+			q.push(now->rson);
+		ans[num]=now->value;
+		num++;
+	}
+	for(int i=0;i<num-1;i++)
+		cout<<ans[i]<<" ";
+	cout<<ans[num-1]<<endl;
+}
+
 int main()
 {
 	scanf("%d",&n);
@@ -83,11 +116,17 @@ int main()
 	int i,j;
 	for(i=0;i<n;i++)
 		scanf("%d",&afterorder[i]);
+//	for(i=0;i<n;i++)
+//		scanf("%d",&preorder[i]);
 	for(i=0;i<n;i++)
 		scanf("%d",&inorder[i]);
-	cout<<"hhh\n";
+	//cout<<"hhh\n";
+	
+	//建立 
 	root=BinaryTreeOrderings1(n-1,0,n);
-	cout<<"hh\n";
-	preordering(root);
+	//preordering(root);
+	Cengordering(root);
+//	root=BinaryTreeOrderings2(0,0,n);
+//	afterordering(root);
 	return 0;
 }
